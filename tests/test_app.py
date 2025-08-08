cc62a2a8481070774d7b6fc0e5178aed3aa8280d
aqsatariq102 <iaqsa102@gmail.com>
2025-08-08 15:15:30 +0500

    update test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_basic():
+    # nosec B101
     assert 2 + 2 == 4
 
-
-def test_dash_server():
+def test_app_import():
     from app import app
+    # nosec B101
     assert app is not None
+    # nosec B101
     assert app.server is not None
